349d8d41cdceaa8a04b6b35d52dc22bd6adaa840
RyanTruongg <thanhnhut.02jan@gmail.com>
2022-03-28 09:16:49 +0700

    Initial version of jconsumer generated by generator-jhipster@7.7.0

---
+++ jconsumer/src/test/java/com/leap/jconsumer/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.leap.jconsumer;
+
+import com.leap.jconsumer.JconsumerApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = JconsumerApp.class)
+public @interface IntegrationTest {
+}
