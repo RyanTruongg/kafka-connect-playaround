f783b66b6e6250b5b284ed843a39d691e1d95388
RyanTruongg <thanhnhut.02jan@gmail.com>
2022-03-29 17:04:14 +0700

    add java consumer

---
+++ jconsumer/src/main/java/com/leap/jconsumer/service/JConsumer.java
@@ -0,0 +1,80 @@
+package com.leap.jconsumer.service;
+
+import com.leap.jconsumer.config.KafkaProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Service
+public class JConsumer {
+    private final Logger log = LoggerFactory.getLogger(JConsumer.class);
+
+    private final AtomicBoolean closed = new AtomicBoolean(false);
+
+    public static final String TOPIC = "postgres.public.student";
+
+    private final KafkaProperties kafkaProperties;
+
+    private KafkaConsumer<String, String> kafkaConsumer;
+
+    private ExecutorService executorService = Executors.newCachedThreadPool();
+
+    public JConsumer(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    @PostConstruct
+    public void start() {
+
+        log.info("Kafka consumer starting...");
+        this.kafkaConsumer = new KafkaConsumer<>(kafkaProperties.getConsumerProps());
+        Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown));
+        kafkaConsumer.subscribe(Collections.singletonList(TOPIC));
+        log.info("Kafka consumer started");
+
+        executorService.execute(() -> {
+            try {
+                while (!closed.get()) {
+                    ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofSeconds(3));
+                    for (ConsumerRecord<String, String> record : records) {
+                        log.info("Consumed message in {} : {}", TOPIC, record.value());
+
+                    }
+                }
+                kafkaConsumer.commitSync();
+            } catch (WakeupException e) {
+                // Ignore exception if closing
+                if (!closed.get())
+                    throw e;
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            } finally {
+                log.info("Kafka consumer close");
+                kafkaConsumer.close();
+            }
+        });
+    }
+
+    public KafkaConsumer<String, String> getKafkaConsumer() {
+        return kafkaConsumer;
+    }
+
+    public void shutdown() {
+        log.info("Shutdown Kafka consumer");
+        closed.set(true);
+        kafkaConsumer.wakeup();
+    }
+}
